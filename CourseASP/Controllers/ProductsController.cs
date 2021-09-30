246a840184a8f3cd1691cbbfc8e38456f93447ab
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-30 16:32:05 -0300

    Adicionar arquivos de projeto.

---
+++ CourseASP/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using CourseASP.Models;
+using CourseASP.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseASP.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+        [HttpGet]
+
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+
+
+    }
+}
